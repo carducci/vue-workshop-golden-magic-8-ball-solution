fbf6bdf6b55f14b4082b961f09ebc687097d2a55
Carducci <michael@mago.co>
2019-09-03 21:08:12 -0500

    full/final workshop demo

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <TheBall></TheBall>
+</template>
+
+<script>
+import TheBall from "@/components/TheBall";
+export default {
+  components:{
+    TheBall
+  }
+}
+</script>
+
+<style>
+
+</style>
